287aa7676317f501af7cf952ee46b650fba4d603
exerro <benedict.allen2514@gmail.com>
2023-01-28 13:37:07 +0000

    Add hello triangle example code

---
+++ examples/hello_triangle.lua
@@ -0,0 +1,21 @@
+
+-- Import the library
+local v3d = require '/v3d'
+
+-- Create objects using default settings
+local framebuffer = v3d.create_framebuffer_subpixel(term.getSize())
+local geometry = v3d.create_geometry(v3d.GEOMETRY_COLOUR)
+local camera = v3d.create_camera()
+local pipeline = v3d.create_pipeline()
+
+-- Add a red triangle
+geometry:add_colour_triangle(0, 0.8, -1, -0.9, -0.8, -1, 0.9, -0.8, -1, colours.red)
+
+-- Clear the framebuffer to light blue
+framebuffer:clear(colours.lightBlue)
+
+-- Draw the geometry to the framebuffer
+pipeline:render_geometry({ geometry }, framebuffer, camera)
+
+-- Draw the framebuffer to the screen
+framebuffer:blit_subpixel(term)
