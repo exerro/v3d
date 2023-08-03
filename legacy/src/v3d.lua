d7674e4b6eff09d18d2e9083d02a16ba09288af3
exerro <benedict.allen2514@gmail.com>
2023-08-03 18:01:56 +0100

    Move legacy stuff out of base repo

---
+++ legacy/src/v3d.lua
@@ -0,0 +1,11 @@
+
+-- TODO: make V3DGeometryBuilder be able to splice attributes
+-- TODO: v3d.shaders.write_constant()
+-- TODO: v3d.shaders.write_attribute()
+-- TODO: v3d.shaders.write_uniform()
+-- TODO: v3d.shaders.depth_test()
+-- TODO: v3d.shaders.combine()
+
+-- #remove
+return {}
+-- #end
